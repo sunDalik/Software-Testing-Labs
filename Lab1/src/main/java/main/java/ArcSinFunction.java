484fa56475318aa832244d741a887a91ad7ad3cb
sunDalik <dali1999@mail.ru>
2020-09-20 00:16:48 +0300

    Lab1: arcsin

---
+++ Lab1/src/main/java/main/java/ArcSinFunction.java
@@ -0,0 +1,24 @@
+package main.java;
+
+public class ArcSinFunction {
+    public static double precision = 1E-10;
+
+    // power series implementation
+    public static Double getArcSin(double x) {
+        if (Math.abs(x) > 1) return null;
+        if (x == 1.0) return Math.PI / 2;
+        if (x == -1.0) return -Math.PI / 2;
+
+        double currentTerm = x;
+        double result = 0.0;
+        int n = 1;
+
+        while (Math.abs(currentTerm) >= precision) {
+            result += currentTerm;
+            currentTerm = currentTerm * x * x * (2 * n - 1) * (2 * n - 1) / ((2 * n) * (2 * n + 1));
+            n++;
+        }
+
+        return result;
+    }
+}
---
+++ Lab1/src/test/java/test/java/ArcSinTest.java
@@ -0,0 +1,63 @@
+package test.java;
+
+
+import main.java.ArcSinFunction;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class ArcSinTest {
+
+    private static DecimalFormat df;
+
+    @BeforeClass
+    public static void testSetup() {
+        df = new DecimalFormat("#.######");
+        df.setRoundingMode(RoundingMode.CEILING);
+    }
+
+    @Test
+    public void testAt0() {
+        testWithValue(0);
+    }
+
+    @Test
+    public void testBorders() {
+        testWithValue(-1);
+        testWithValue(1);
+    }
+
+    @Test
+    public void testLinear() {
+        testWithValue(-0.3);
+        testWithValue(0.3);
+        testWithValue(-0.5);
+        testWithValue(0.5);
+    }
+
+    @Test
+    public void testCurve() {
+        testWithValue(-0.97);
+        testWithValue(0.97);
+    }
+
+    @Test
+    public void testOutOfBounds() {
+        testAssertNull(-2);
+        testAssertNull(2);
+    }
+
+    private void testAssertNull(double value) {
+        Double actual = ArcSinFunction.getArcSin(value);
+        Assert.assertNull("Result should be null for x = " + value, actual);
+    }
+
+    private void testWithValue(double value) {
+        String expected = df.format(Math.asin(value));
+        String actual = df.format(ArcSinFunction.getArcSin(value));
+        Assert.assertEquals("Wrong result for x = " + value, expected, actual);
+    }
+}
