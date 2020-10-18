5f5121e733f8c2b34dd51ebdcedf45e127ceca56
sunDalik <dali1999@mail.ru>
2020-10-18 13:56:46 +0300

    Add logarithm tests

---
+++ Lab2/src/test/java/LogarithmEvaluatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class LogarithmEvaluatorTest {
+    public static final double EPS = 1e-6;
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/logarithm_test.csv", numLinesToSkip = 1)
+    public void testLn(Double x, Double base, Double lnX, Double lnBase, Double logBaseX) {
+        Assertions.assertEquals(lnX, LogarithmEvaluator.ln(x, EPS), EPS);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/logarithm_test.csv", numLinesToSkip = 1)
+    public void testLog(Double x, Double base, Double lnX, Double lnBase, Double logBaseX) {
+        Assertions.assertEquals(logBaseX, LogarithmEvaluator.log(x, base, EPS), EPS);
+    }
+}
--- Lab2/src/test/java/SinFunctionTest.java
+++ Lab2/src/test/java/SinFunctionTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class SinFunctionTest {
