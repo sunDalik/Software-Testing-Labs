f10d4a66c683a3d1438760fd9594c7dd22d30997
sunDalik <dali1999@mail.ru>
2020-10-30 16:15:37 +0300

    Add driver provider

--- Lab3/src/test/java/LoginTest.java
+++ Lab3/src/test/java/LoginTest.java
@@ -1,10 +1,11 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import utils.DriverProvider;
 
 public class LoginTest {
-
     @Test
     public void loginTest() {
-        System.out.print(Credentials.login + " =) " + Credentials.password);
+        WebDriver driver = DriverProvider.getDriver();
+        driver.get("https://timeweb.com");
     }
 }
---
+++ Lab3/src/test/java/utils/DriverProvider.java
@@ -0,0 +1,41 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+enum DRIVER_TYPE {
+    CHROME,
+    FIREFOX
+}
+
+public class DriverProvider {
+    private static DRIVER_TYPE driverType;
+
+    static {
+        InputStream inputStream = Credentials.class.getClassLoader().getResourceAsStream("driver");
+        Scanner s = new Scanner(inputStream).useDelimiter("\n");
+        String driverTypeString = s.next();
+        if (driverTypeString.toLowerCase().equals("firefox")) {
+            driverType = DRIVER_TYPE.FIREFOX;
+        } else {
+            driverType = DRIVER_TYPE.CHROME;
+        }
+        System.setProperty("webdriver.chrome.driver", s.next());
+        System.setProperty("webdriver.gecko.driver", s.next());
+    }
+
+    public static WebDriver getDriver() {
+        WebDriver driver;
+        if (driverType == DRIVER_TYPE.FIREFOX) {
+            driver = new FirefoxDriver();
+        } else {
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+}
