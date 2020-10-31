c92b565a3111a5167bb212dabae5c3203b473c53
sunDalik <dali1999@mail.ru>
2020-10-31 14:46:15 +0300

    Add admin creation test

--- Lab3/src/test/java/AdminTest.java
+++ Lab3/src/test/java/AdminTest.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.DriverProvider;
 import webpages.AdministratorsPage;
 import webpages.LoginPage;
@@ -9,8 +14,17 @@ public class AdminTest {
     public void adminCreationTest() {
         WebDriver driver = DriverProvider.getDriver();
         new LoginPage(driver, true).loginAndWait();
+
         AdministratorsPage adminPage = new AdministratorsPage(driver, true);
-        adminPage.createRandomAdmin();
+        int adminTableRowsCount = driver.findElements(By.xpath("//div[@id='w0']/table/tbody/tr")).size();
+        String adminName = adminPage.createRandomAdmin();
+
+        // wait until new entry is put in the admin table
+        new WebDriverWait(driver, 10).until(ExpectedConditions.numberOfElementsToBe(By.xpath("//div[@id='w0']/table/tbody/tr"), adminTableRowsCount + 1));
+
+        // check that our new admin is put in the last row of the admin table
+        WebElement newAdminNameCell = driver.findElement(By.xpath("//div[@id='w0']/table/tbody/tr[last()]/td[1]"));
+        Assertions.assertEquals(adminName, newAdminNameCell.getText());
         driver.quit();
     }
 }
